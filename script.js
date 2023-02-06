72c6232b2e7d465b4a60b044c6d90e7063165980
Ida Svale <idasvale@gmail.com>
2023-02-06 02:07:42 +0100

    validation

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-//nu laver vi en funktion. =() => betyder en anonym funktion.
-//vi tager fat i burgeren og navigation. Det der skal ske nu er, at når man trykker
-//på burgermenuen så vil vi have at nav-links skal have classen ".nav.active"
-//
-
-//for at få fat på de individuelle overskrifter og få dem ind på nav-slide-boxen
-//tager man fat i navLinks.
-
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
 
-  //toggle nav
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
 
